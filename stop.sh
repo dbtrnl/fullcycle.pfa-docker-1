ed83f87f0bfe5be9349f18a149f9e32d6193917b
dbtrnl <daniel.bittar@alumni.usp.br>
2021-04-27 20:45:28 -0300

    adding error treatment to stop.sh

--- stop.sh
+++ stop.sh
@@ -1,8 +1,40 @@
+# Setting color variables to customize the output
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+echo
 echo "Stopping container..."
 docker stop pfa-mysql
+
+if [ $? -ne 0 ]
+  then
+    echo "${red}Error stopping container. Already stopped?${reset}"
+  else
+    echo "${green}Container stopped...${reset}"
+fi
+
+echo
+echo "Removing container..."
 docker rm pfa-mysql
+
+if [ $? -ne 0 ]
+  then
+    echo "${red}Error during container removal. Already deleted?${reset}"
+  else
+    echo "${green}Container deleted...${reset}"
+fi
+
 echo
 echo "Removing network..."
 docker network rm pfa
+
+if [ $? -ne 0 ]
+  then
+    echo "${red}Error during container removal. Already deleted?${reset}"
+  else
+    echo "${green}Network deleted...${reset}"
+fi
 echo
-echo "Done."
+echo "${green}Done.${reset}"
+echo
