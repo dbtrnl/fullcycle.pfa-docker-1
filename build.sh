9957326e38ef7fd4a6f73c969bcbfb33646ecc19
dbtrnl <daniel.bittar@alumni.usp.br>
2021-04-27 21:55:56 -0300

    changes to build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,21 @@
-echo "Removing images..."
-docker image rm dnbtr/pfa-node
-docker image rm dnbtr/pfa-mysql
-docker image rm dnbtr/pfa-nginx
+# Setting color variables to customize the output
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+echo "${green}Removing images...${reset}"
+docker image rm dnbtr/pfa-node > /dev/null 2>&1
+docker image rm dnbtr/pfa-mysql > /dev/null 2>&1
+docker image rm dnbtr/pfa-nginx > /dev/null 2>&1
 
 echo
-echo "Building pfa-node..."
-docker build -t dnbtr/pfa-node ./node
+echo "${green}Building pfa-node..."
+docker build -t dnbtr/pfa-node ./node > /dev/null 2>&1
 echo
 echo "Building pfa-mysql..."
-docker build -t dnbtr/pfa-mysql ./mysql
+docker build -t dnbtr/pfa-mysql ./mysql > /dev/null 2>&1
 echo
 echo "Building pfa-nginx..."
-docker build -t dnbtr/pfa-nginx ./nginx
+docker build -t dnbtr/pfa-nginx ./nginx > /dev/null 2>&1
 echo
-echo "Done."
+echo "Done.${reset}"
