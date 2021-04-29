90014669121eb9f5120ba7302228d643716e3f06
dbtrnl <daniel.bittar@alumni.usp.br>
2021-04-28 22:22:10 -0300

    Improvements to start.sh and stop.sh

--- start.sh
+++ start.sh
@@ -1,101 +1,99 @@
 # Setting color variables to customize the output
-red=`tput setaf 1`
+red=`tput setaf 9`
 green=`tput setaf 2`
+yellow=`tput setaf 3`
 reset=`tput sgr0`
 
+function text_to_grey () {
+  tput setaf 240
+}
+
+function error_treatment () {
+  if [ $? -ne 0 ]
+    then
+      echo "${red}ERROR${reset} on $current_action${reset}. Exiting..."
+      echo
+      exit 1
+    else
+      echo "${reset}$current_action...${green} SUCCESS${reset}"
+  fi
+}
+
+function countdown_fifteen () {
+  for count in $(seq 0 15);
+  do
+    echo -ne "$count seconds\033[0K\r"
+    sleep 1
+  done
+}
+
+function countdown_five () {
+  for count in $(seq 0 5);
+  do
+    echo -ne "$count seconds\033[0K\r"
+    sleep 1
+  done
+}
+
+# Creating docker network
+current_action="Network creation"
 echo
 echo "Creating network named 'pfa'..."
+text_to_grey
 docker network create pfa
+error_treatment
 
-# Error treatment
-if [ $? -ne 0 ]
-  then
-    echo "${red}Error during network creation. Exiting...${reset}"
-    echo
-    exit 1
-  else
-    echo "${green}Network created!${reset}"
-fi
-
+# Creating MySQL container
+current_action="MySQL container creation"
 echo
 echo "Creating database container..."
+text_to_grey
 docker run --name pfa-mysql -h 127.0.0.1 -p 3306 --network=pfa -d dnbtr/pfa-mysql
+error_treatment
 
-# Error treatment
-if [ $? -ne 0 ]
-  then
-    echo "${red}Error during container creation. Exiting...${reset}"
-    echo
-    exit 1
-  else
-    echo "${green}Container created!${reset}"
-fi
-
+# Wait until MySQL is ready for connections...
 echo
-echo "Waiting until database is ready..."
-sleep 15
-# Wait until the Database is started inside the container
-# The output messages are supressed.
+echo "Waiting 15 seconds until database is ready..."
+countdown_fifteen
 until docker exec -i pfa-mysql mysql -uroot -pmysql -e "show databases" > /dev/null 2>&1
 do
   echo "Database not yet ready. Waiting 5 secs to try again..."
-  sleep 5
+  countdown_five
 done
 
 # Populate the database
+current_action="Database population"
 echo
 echo "Populating the database..."
+text_to_grey
 docker exec -i pfa-mysql mysql -pmysql < ./mysql/db-setup.sql > /dev/null 2>&1
-
-if [ $? -ne 0 ]
-  then
-    echo "${red}Error while populating the database. Exiting...${reset}"
-    echo
-    exit 1
-  else
-    echo "${green}Database sucessfully populated!${reset}"
-fi
+error_treatment
 
 # Querying the database and supressing warning message...
+current_action="Database test query"
 echo
 echo "Querying data on the database..."
 echo "SELECT COUNT(*) FROM fc_modules;"
+text_to_grey
 docker exec -i pfa-mysql mysql -uroot -pmysql -e "use fc; select count(*) from fc_modules;" 2>&1 | grep -v "Warning"
+error_treatment
 
-if [ $? -ne 0 ]
-  then
-    echo "${red}Error making the query. Exiting...${reset}"
-    echo
-    exit 1
-  else
-    echo "${green}Test query sucessful.${reset}"
-fi
-
+# Starting NodeJS container
+current_action="NodeJS container initialization"
 echo
 echo "Starting NodeJS container..."
+text_to_grey
 docker run -d --name=pfa-node --network=pfa dnbtr/pfa-node npm run start
+error_treatment
 
-if [ $? -ne 0 ]
-  then
-    echo "${red}Error starting NodeJS container. Exiting...${reset}"
-    echo
-    exit 1
-  else
-    echo "${green}NodeJS container started!.${reset}"
-fi
-
+# Starting Nginx container
+current_action="Nginx reverse shell container initialization"
 echo
 echo "Starting nginx reverse shell..."
+text_to_grey
 docker run -d --name=pfa-nginx --network=pfa -p 8888:80 dnbtr/pfa-nginx
+error_treatment
 
-if [ $? -ne 0 ]
-  then
-    echo "${red}Error starting nginx reverse shell. Exiting...${reset}"
-    echo
-    exit 1
-  else
-    echo "${green}Nginx reverse shell started!."
-fi
-
-echo "${green}All done! Test a request to localhost:8888/fc_modules${reset}"
+echo
+echo "${yellow}All done! Test a request to localhost:8888/fc_modules${reset}"
 echo
--- stop.sh
+++ stop.sh
@@ -1,56 +1,67 @@
 # Setting color variables to customize the output
 red=`tput setaf 1`
 green=`tput setaf 2`
+yellow=`tput setaf 3`
 reset=`tput sgr0`
 
+function text_to_grey () {
+  tput setaf 240
+}
+
+function error_treatment () {
+  if [ $? -ne 0 ]
+    then
+      echo "${red}ERROR${reset} on ${yellow}$current_action${reset} removal. Already deleted?"
+    else
+      echo "${green}$current_action deleted...${reset}"
+  fi
+}
+
+# Remove nginx container
+current_action="Nginx container"
 echo
-echo "Stopping and removing Nginx container..."
+echo "Stopping and removing $current_action..."
+text_to_grey
 docker rm pfa-nginx -f
+error_treatment
 
-if [ $? -ne 0 ]
-  then
-    echo "${red}Error during container removal. Already deleted?${reset}"
-  else
-    echo "${green}Nginx container deleted...${reset}"
-fi
-
+# Remove MySQL container
+current_action="MySQL container"
 echo
-echo "Stopping and removing MySQL container..."
+echo "Stopping and removing $current_action..."
+text_to_grey
 docker rm pfa-mysql -f
+error_treatment
 
-if [ $? -ne 0 ]
-  then
-    echo "${red}Error during container removal. Already deleted?${reset}"
-  else
-    echo "${green}MySQL container deleted...${reset}"
-fi
-
+# Remove NodeJS container
+current_action="NodeJS container"
 echo
-echo "Stopping and removing NodeJS container..."
+echo "Stopping and removing $current_action..."
+text_to_grey
 docker rm pfa-node -f
+error_treatment
 
-if [ $? -ne 0 ]
-  then
-    echo "${red}Error during container removal. Already deleted?${reset}"
-  else
-    echo "${green}NodeJS container deleted...${reset}"
-fi
-
+# Remove docker network
+current_action="Docker network"
 echo
 echo "Removing network..."
+text_to_grey
 docker network rm pfa
+error_treatment
 
-if [ $? -ne 0 ]
-  then
-    echo "${red}Error during container removal. Already deleted?${reset}"
-  else
-    echo "${green}Network deleted...${reset}"
-fi
+# Remove images
 echo
-echo "${green}Removing images...${reset}"
-docker image rm dnbtr/pfa-node > /dev/null 2>&1
-docker image rm dnbtr/pfa-mysql > /dev/null 2>&1
-docker image rm dnbtr/pfa-nginx > /dev/null 2>&1
+read -p "Do you want to remove the images created on start.sh? (y/N)" -n 1 -r choice
+echo
+if [[ $choice =~ ^[Yy]$ ]]
+  then
+    echo "Removing images..."
+    docker image rm dnbtr/pfa-node > /dev/null 2>&1
+    docker image rm dnbtr/pfa-mysql > /dev/null 2>&1
+    docker image rm dnbtr/pfa-nginx > /dev/null 2>&1
+  else
+    echo "Images won't be removed."
+fi
 
 echo
 echo "${green}Done.${reset}"
