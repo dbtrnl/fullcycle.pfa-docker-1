53f4c677b461acc2855206cb631fd92369a8204f
dbtrnl <daniel.bittar@alumni.usp.br>
2021-06-04 01:13:13 -0300

    Refactoring 'start' and 'stop' scripts

--- start.sh
+++ start.sh
@@ -4,92 +4,86 @@ green=`tput setaf 2`
 yellow=`tput setaf 3`
 reset=`tput sgr0`
 
+# Docker text to grey
 function text_to_grey () {
   tput setaf 240
 }
 
+# Treats errors and also resets text color
 function error_treatment () {
   if [ $? -ne 0 ]
     then
-      echo "${red}ERROR${reset} on $current_action${reset}. Exiting..."
-      echo
+      echo "${red}ERROR${reset} on $current_action${reset}. Exiting...
+      "
       exit 1
     else
       echo "${reset}$current_action...${green} SUCCESS${reset}"
   fi
 }
 
-function countdown_fifteen () {
-  for count in $(seq 0 15);
+function countdown () {
+  for count in $(seq 0 $1);
   do
-    echo -ne "$count seconds\033[0K\r"
+    echo -ne "$count seconds\033[0K\r"    # Resets the line so "X seconds" isn't printed multiple times
     sleep 1
   done
-}
-
-function countdown_five () {
-  for count in $(seq 0 5);
-  do
-    echo -ne "$count seconds\033[0K\r"
-    sleep 1
-  done
+  echo -ne "\033[0K\r"
 }
 
 # Creating docker network
 current_action="Network creation"
-echo
-echo "Creating network named 'pfa'..."
+echo "
+Creating network named 'pfa'..."
 text_to_grey
 docker network create pfa
 error_treatment
 
 # Creating MySQL container
 current_action="MySQL container creation"
-echo
-echo "Creating database container..."
+echo "
+Creating database container..."
 text_to_grey
 docker run --name pfa-mysql -h 127.0.0.1 -p 3306 --network=pfa -d dnbtr/pfa-mysql
 error_treatment
 
 # Wait until MySQL is ready for connections...
-echo
-echo "Waiting 15 seconds until database is ready..."
-countdown_fifteen
+echo "
+Waiting 15 seconds until database is ready..."
+countdown 1
 until docker exec -i pfa-mysql mysql -uroot -pmysql -e "show databases" > /dev/null 2>&1
 do
   echo "Database not yet ready. Waiting 5 secs to try again..."
-  countdown_five
+  countdown 5
 done
 
 # Populate the database
 current_action="Database population"
-echo
-echo "Populating the database..."
+echo -n "
+Populating the database... "
 text_to_grey
 docker exec -i pfa-mysql mysql -pmysql < ./mysql/db-setup.sql > /dev/null 2>&1
 error_treatment
 
 # Querying the database and supressing warning message...
 current_action="Database test query"
-echo
-echo "Querying data on the database..."
-echo "SELECT COUNT(*) FROM fc_modules;"
+echo "Querying data on the database...
+SELECT COUNT(*) FROM fc_modules;"
 text_to_grey
 docker exec -i pfa-mysql mysql -uroot -pmysql -e "use fc; select count(*) from fc_modules;" 2>&1 | grep -v "Warning"
 error_treatment
 
 # Starting NodeJS container
 current_action="NodeJS container initialization"
-echo
-echo "Starting NodeJS container..."
+echo "
+Starting NodeJS container..."
 text_to_grey
 docker run -d --name=pfa-node --network=pfa dnbtr/pfa-node npm run start
 error_treatment
 
 # Starting Nginx container
 current_action="Nginx reverse shell container initialization"
-echo
-echo "Starting nginx reverse shell..."
+echo "
+Starting nginx reverse shell..."
 text_to_grey
 docker run -d --name=pfa-nginx --network=pfa -p 8888:80 dnbtr/pfa-nginx
 error_treatment
--- stop.sh
+++ stop.sh
@@ -19,50 +19,51 @@ function error_treatment () {
 
 # Remove nginx container
 current_action="Nginx container"
-echo
-echo "Stopping and removing $current_action..."
+echo "
+Stopping and removing $current_action..."
 text_to_grey
 docker rm pfa-nginx -f
 error_treatment
 
 # Remove MySQL container
 current_action="MySQL container"
-echo
-echo "Stopping and removing $current_action..."
+echo "
+Stopping and removing $current_action..."
 text_to_grey
 docker rm pfa-mysql -f
 error_treatment
 
 # Remove NodeJS container
 current_action="NodeJS container"
-echo
-echo "Stopping and removing $current_action..."
+echo "
+Stopping and removing $current_action..."
 text_to_grey
 docker rm pfa-node -f
 error_treatment
 
 # Remove docker network
 current_action="Docker network"
-echo
-echo "Removing network..."
+echo "
+Removing network..."
 text_to_grey
 docker network rm pfa
 error_treatment
 
 # Remove images
-echo
-read -p "Do you want to remove the images created on start.sh? (y/N)" -n 1 -r choice
+read -p "
+Do you want to remove the images created on start.sh? (y/N) " -n 1 -r choice
 echo
 if [[ $choice =~ ^[Yy]$ ]]
   then
     echo "Removing images..."
-    docker image rm dnbtr/pfa-node > /dev/null 2>&1
-    docker image rm dnbtr/pfa-mysql > /dev/null 2>&1
-    docker image rm dnbtr/pfa-nginx > /dev/null 2>&1
+    text_to_grey
+    docker image rm dnbtr/pfa-node 
+    docker image rm dnbtr/pfa-mysql
+    docker image rm dnbtr/pfa-nginx
   else
     echo "Images won't be removed."
 fi
 
-echo
-echo "${green}Done.${reset}"
-echo
+echo "
+${green}All done.${reset}
+"
